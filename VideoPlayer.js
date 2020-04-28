77af81859f3a1b0faaf0887af5801431c0aa491d
Felipe Santos <felipemillhouse@gmail.com>
2020-04-28 14:53:51 -0400

    INTDEV-2808 minor fix

--- VideoPlayer.js
+++ VideoPlayer.js
@@ -1048,14 +1048,14 @@ export default class VideoPlayer extends Component {
             style={[
               styles.seekbar.fill,
               {
-                width: this.state.seekerFillWidth,
+                width: this.state.seekerFillWidth || 0,
                 backgroundColor: this.props.seekColor || '#FFF'
               }
             ]}
           />
         </View>
         <View
-          style={[styles.seekbar.handle, { left: this.state.seekerPosition }]}
+          style={[styles.seekbar.handle, { left: this.state.seekerPosition || 0 }]}
           {...this.player.seekPanResponder.panHandlers}>
           <View
             style={[
